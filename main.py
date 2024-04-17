a6d96132fc47392bc6ff0356ff0a79d7c94e89d5
ismailmoufid47 <144567786+ismailmoufid47@users.noreply.github.com>
2024-04-17 21:27:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,52 +6,54 @@ from datetime import datetime
 from twitter_scraper_selenium import get_profile_details
 from requests import request
 
-
-post_count = 6119
 interval = 10 #seconds
 
 pb = API()
 pb.set_token('Your Pushbullet api key')
+
 url = "{{APIUrl}}/waInstance{{idInstance}}/sendMessage/{{apiTokenInstance}}"
 headers = {
 'Content-Type': 'application/json'
 }
 
+def get_post_count():
+  profile = loads(get_profile_details('1337FIL'))
+  statuses_count = profile['user']['result']['legacy']['statuses_count']
+  return statuses_count
+  
+latest_post_count = get_post_count()
+
 def check():
-    global post_count
+    global latest_post_count
     global interval
     current_time = datetime.now()
     if current_time.hour >= 10:
         try:
-            profile = loads(get_profile_details('1337FIL'))
-            statuses_count = profile['user']['result']['legacy']['statuses_count']
+            statuses_count = get_post_count()
             
-            if statuses_count > post_count:
+            if statuses_count > latest_post_count:
                 pb.send_link("1337 Posted", "Hurry!", "https://candidature.1337.ma/meetings")
                 request("POST", url, headers=headers, data = payload)
                 request("POST", url, headers=headers, data = payload)
                 request("POST", url, headers=headers, data = payload)
                 payload = "{\r\n\t\"chatId\": \"120363286086474982@g.us\",\r\n\t\"message\": \"this message was sent by a bot\"\r\n}"
-                
                 request("POST", url, headers=headers, data = payload)
-                post_count += 1
+                latest_post_count += 1
                 print(datetime.now(), ': Posted, checking again in ', interval, 'seconds')
-            elif statuses_count < post_count:
+            elif statuses_count < latest_post_count:
                 print(datetime.now(), ': deleted post, checking again in ', interval, 'seconds')
-                post_count = statuses_count
+                latest_post_count = statuses_count
             else:
                 print(datetime.now(), ": Haven't posted, checking again in ", interval, "seconds")
         except Exception as e:
             print("Error: ", e)
             pb.send_note("Caution, Error", str(e))
             
-        # Start the timer again with the updated interval
         Timer(interval, check).start()
     else:
         time_until_10 = (current_time.replace(hour=10, minute=0, second=0, microsecond=0) - current_time).total_seconds()
         print("Past midnight, checking again until 10am in:", time_until_10, "seconds")
-        sleep(time_until_10)
-        check()
+        Timer(time_until_10, check).start()
 
 if __name__ == "__main__":
     check()
