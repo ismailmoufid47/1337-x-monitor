55d56d98e455092e48927f94e702664d974ba418
ismailmoufid47 <144567786+ismailmoufid47@users.noreply.github.com>
2024-04-19 22:17:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,42 +16,69 @@ headers = {
 }
 
 def get_post_count():
-  profile = loads(get_profile_details('1337FIL'))
-  statuses_count = profile['user']['result']['legacy']['statuses_count']
-  return statuses_count
-  
+    profile = loads(get_profile_details("1337FIL"))
+    statuses_count = profile["user"]["result"]["legacy"]["statuses_count"]
+    return statuses_count
+
+
 latest_post_count = get_post_count()
+
 
 def check():
     global latest_post_count
     current_time = datetime.now()
+
     if current_time.hour >= 10:
         try:
             statuses_count = get_post_count()
-            
+
             if statuses_count > latest_post_count:
-                pb.send_link("1337 Posted", "Hurry!", "https://candidature.1337.ma/meetings")
-                request("POST", url, headers=headers, data = payload)
-                request("POST", url, headers=headers, data = payload)
-                request("POST", url, headers=headers, data = payload)
-                payload = "{\r\n\t\"chatId\": \"120363286086474982@g.us\",\r\n\t\"message\": \"this message was sent by a bot\"\r\n}"
-                request("POST", url, headers=headers, data = payload)
-                latest_post_count += 1
-                print(datetime.now(), ': Posted, checking again in ', interval, 'seconds')
-            elif statuses_count < latest_post_count:
-                print(datetime.now(), ': deleted post, checking again in ', interval, 'seconds')
+                pb.send_link(
+                    "1337 Posted", "Hurry!", "https://candidature.1337.ma/meetings"
+                )
+
+                request("POST", url, headers=headers, data=payload)
+                request("POST", url, headers=headers, data=payload)
+                request("POST", url, headers=headers, data=payload)
+                request(
+                    "POST",
+                    url,
+                    headers=headers,
+                    data='{\r\n\t"chatId": "120363286086474982@g.us",\r\n\t"message": "this message was sent by a bot"\r\n}',
+                )
+
                 latest_post_count = statuses_count
+                print(
+                    datetime.now(), ": Posted, checking again in ", interval, "seconds"
+                )
+            elif statuses_count < latest_post_count:
+                latest_post_count = statuses_count
+                print(
+                    datetime.now(),
+                    ": deleted post, checking again in ",
+                    interval,
+                    "seconds",
+                )
             else:
-                print(datetime.now(), ": Haven't posted, checking again in ", interval, "seconds")
+                print(
+                    datetime.now(),
+                    ": Haven't posted, checking again in ",
+                    interval,
+                    "seconds",
+                )
         except Exception as e:
             print("Error: ", e)
             pb.send_note("Caution, Error", str(e))
-            
+
         Timer(interval, check).start()
     else:
-        time_until_10 = (current_time.replace(hour=10, minute=0, second=0, microsecond=0) - current_time).total_seconds()
+        time_until_10 = (
+            current_time.replace(hour=10, minute=0, second=0, microsecond=0)
+            - current_time
+        ).total_seconds()
         print("Past midnight, checking again until 10am in:", time_until_10, "seconds")
         Timer(time_until_10, check).start()
+
 
 if __name__ == "__main__":
     check()
